075aa617302970c70a1263c3a56a21293083c5a2
Sedat2121 <93882574+Sedat2121@users.noreply.github.com>
2022-11-21 14:38:50 +0000

    wrote some js test code

---
+++ Day 15/script.js
@@ -0,0 +1,11 @@
+console.log("first class in js")
+
+console.log("45")
+console.log(45)
+// the above numbers would be different in types.
+
+//short cut: cgl and hit enter to wrote console.log()
+
+console.warn("this is a warning message")
+console.error("this is an error message")
+
