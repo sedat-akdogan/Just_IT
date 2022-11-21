8e88e715d17dd01809509e249cec8d93be65a07f
Sedat2121 <93882574+Sedat2121@users.noreply.github.com>
2022-11-21 16:41:50 +0000

    var, let and const are instodured

--- Day 15/script.js
+++ Day 15/script.js
@@ -1,11 +1,23 @@
-console.log("first class in js")
+// console.log("first class in js")
 
-console.log("45")
-console.log(45)
-// the above numbers would be different in types.
+// console.log("45")
+// console.log(45)
+// // the above numbers would be different in types.
 
-//short cut: cgl and hit enter to wrote console.log()
+// //short cut: cgl and hit enter to wrote console.log()
 
-console.warn("this is a warning message")
-console.error("this is an error message")
+// console.warn("this is a warning message")
+// console.error("this is an error message")
+
+// var age = 38;
+// var name = "alperen";
+
+// console.log(age)
+
+
+// we cannot reassign a const variable!
+const number = 11;
+number = 33;
+
+// this will lead a typeError
 
