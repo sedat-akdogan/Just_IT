b706b1154cdb5418487cc7f8509d2427cb4d3e4e
Sedat2121 <93882574+Sedat2121@users.noreply.github.com>
2022-11-22 14:54:06 +0000

    type conversions, parseInt, parseFloat

--- Day 17/script.js
+++ Day 17/script.js
@@ -5,7 +5,45 @@
 
 // console.log(city); // this will lead an error because the city variable is a scope variable.
 
-let num = 19;
-console.log(num++);
-console.log(num);
-console.log(++num);
+// let num = 19;
+// console.log(num++);
+// console.log(num);
+// console.log(++num);
+
+// let num = 33;
+// console.log(num--); //firstly print then decrement
+// console.log(num); //32
+// console.log(--num); // firstly decrement then print
+
+
+// ======================               Data conversion          =====================
+
+// //1. From numbers to strings
+// let num = String(11); // this is a string
+// let num1 = 11; // this is a number
+
+// let num2 = 22;
+// console.log(num2.toString());
+// console.log(typeof num2);
+
+
+// // 2. boolen to strings
+// let boolVar = String(true);
+// console.log(typeof boolVar); // string
+
+// // 3. strings to numbers
+// let num4 = Number ("345.567");
+
+// let num5 = parseInt("678.789"); // it only returns the whole number
+
+
+// let num6 = parseFloat(""); // it only returns the floating point integer
+
+
+// let num7 = 34.567812;
+// console.log(num7.toFixed(3)); // will return 3 decimal points
+
+// let l = "700";
+// let t = 300;
+// console.log(l +t); // this is a string concatenation
+
