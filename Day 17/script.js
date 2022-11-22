0e0c1f8cb8b4deb51f8f205f546f525e054f6ba4
Sedat2121 <93882574+Sedat2121@users.noreply.github.com>
2022-11-22 14:00:55 +0000

    scope variable and increment has seen

---
+++ Day 17/script.js
@@ -0,0 +1,11 @@
+// // scope
+// {
+//     let city = "London";
+// }
+
+// console.log(city); // this will lead an error because the city variable is a scope variable.
+
+let num = 19;
+console.log(num++);
+console.log(num);
+console.log(++num);
