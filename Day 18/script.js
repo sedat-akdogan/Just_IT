7cc838cb56e71f7144fe7e0ce82fe4f160d3b257
Sedat2121 <93882574+Sedat2121@users.noreply.github.com>
2022-11-23 15:33:33 +0000

    switch - case is tested

--- Day 18/script.js
+++ Day 18/script.js
@@ -1,26 +1,68 @@
-console.log("Day 18 work!!");
+// // console.log("Day 18 work!!");
 
-// String immutibility in JS
+// // // String immutibility in JS
 
-// once a string is created, it cannot be changed. 
+// // // once a string is created, it cannot be changed. 
 
-//string methods will create a new string, original string remains unchanged.
+// // //string methods will create a new string, original string remains unchanged.
 
-let str1 = "this is Just IT!";
+// // let str1 = "this is Just IT!";
 
-let str2 = str1.replace("IT", "Apple");
+// // let str2 = str1.replace("IT", "Apple");
 
-console.log(str1);
-console.log(str2);
+// // console.log(str1);
+// // console.log(str2);
 
-str1 = str1.concat(" is it better than Apple!");
-console.log(str1);
-// trim(): it remove the white space from beginning and end of the string.
+// // str1 = str1.concat(" is it better than Apple!");
+// // console.log(str1);
+// // // trim(): it remove the white space from beginning and end of the string.
 
 
-str2 = "         there is some white space among me        ..         ";
-console.log(str2);
-str3 = str2.trim();
+// // str2 = "         there is some white space among me        ..         ";
+// // console.log(str2);
+// // str3 = str2.trim();
 
-console.log(str3);
+// // console.log(str3);
+
+
+
+// // JS Conditionals
+
+// // for form validations
+
+// // age = 24;
+
+// // if (age>18) {
+// //     console.log("you are welcome!");
+// // } else {
+// //     console.log("come with your parent!");
+// // }
+
+// const cup = 40;
+// switch (cup) {
+//     case 10:
+//         console.log("the value is 10")
+//         break;
+//     case 20:
+//         console.log("the value is 20")
+//         break;
+//     case 30:
+//         console.log("the value is 30")
+//         break;
+//     case 40:
+//         console.log("the value is 40")
+//         break;
+//     case 50:
+//         console.log("the value is 50")
+//         break;
+//     case 60:
+//         console.log("the value is 60")
+//         break;
+//     case 70:
+//         console.log("the value is 70")
+//         break;
+//     default:
+//         console.log("there is not a valid value!")
+//         break;
+// }
 
