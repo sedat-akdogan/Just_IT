a8b086fd9d6444b6c2d01e35296aede266dca6ad
Sedat2121 <93882574+Sedat2121@users.noreply.github.com>
2022-11-24 16:29:13 +0000

    Day 19 files are created

---
+++ Day 19/script.js
@@ -0,0 +1,9 @@
+// arrays -  Sorting out arrays:
+
+
+let Arr = [51,66, 78, 99, 23, 55, 5, 6, 8, 9];
+console.log(Arr.sort(function (a, b) {return a-b}));
+
+// function (a, b) {return a-b}; to return numbers in terms of 
+// math increment.
+
