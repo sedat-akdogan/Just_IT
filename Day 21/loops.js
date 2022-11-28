ca5a4f08df81f9d6983c5bf7845c8c2363b14e30
Sedat2121 <93882574+Sedat2121@users.noreply.github.com>
2022-11-28 15:25:08 +0000

    while loop

--- Day 21/loops.js
+++ Day 21/loops.js
@@ -13,3 +13,44 @@
 //     console.log(i);
 // }
 
+// for(i=0; i<=20; i += 4){
+//     console.log(i);
+// }
+
+
+// const animals = ["tiger", "cat", "panda", "wolf", "sheep", "dog"];
+
+// const adj = ["fast", "slow", "flaffy", "dangerous", "delicious", "loyal"];
+
+// for(let i=0; i< animals.length; i++){
+
+//     for(let j=0; j< adj.length; j++){
+    
+//     console.log(animals[i] + " " + adj[j]);
+//     }
+// }
+
+
+
+// let waqasFollowers = ['Abdul', 'Zak', 'Tim'];
+// let narayanFollowers = ['Tim', 'Zak', 'Christian'];
+// let mutualFollowers = [];
+
+// for (let i = 0; i < waqasFollowers.length; i++) { //loop through the first array to get the values
+//     for (let j = 0; j < narayanFollowers.length; j++) { //loop through the second array to get the values
+//         if (waqasFollowers[i] === narayanFollowers[j]) {  // check to see if any values are the same in both arrays
+//             mutualFollowers.push(waqasFollowers[i]);
+//         }
+//     }
+// }
+// console.log(mutualFollowers);
+
+
+
+const phone = ["samsung", "Apple", "Nokia"];
+let i = 0;
+while (i<phone.length){
+    console.log(phone[i]);
+    i++;
+}
+
