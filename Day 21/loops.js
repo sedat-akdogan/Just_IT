3f4e2b8b109d9c948142160d36cb37d4bb7f64ed
Sedat2121 <93882574+Sedat2121@users.noreply.github.com>
2022-11-28 13:55:46 +0000

    for loop

---
+++ Day 21/loops.js
@@ -0,0 +1,15 @@
+// print numbers from 5 to 10 by using for loop:
+
+// for(i=5; i<11; i++) {
+//     console.log(i);
+// }
+
+
+// Make a for loop that loops backwards printing 3 to 0 to 
+// the console. Use the >= comparison operator in your stopping 
+// condition and the - - operator in your iteration statement.
+
+// for(i=3; i>=0; i--) {
+//     console.log(i);
+// }
+
