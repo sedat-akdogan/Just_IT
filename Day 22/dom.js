321b96757778b989a522c4eee14c655e45ee6503
Sedat2121 <93882574+Sedat2121@users.noreply.github.com>
2022-11-29 23:01:43 +0000

    query selector

--- Day 22/dom.js
+++ Day 22/dom.js
@@ -1,8 +1,24 @@
 // Selecting by Tag names
 
-const myEles = document.getElementsByTagName("h1");
-document.getElementById("selected").innerHTML = "your selected greting is: " + myEles[0].innerHTML;
+// const myEles = document.getElementsByTagName("h1");
+// document.getElementById("selected").innerHTML = "your selected greting is: " + myEles[0].innerHTML;
 
-console.log(myEles);
+// console.log(myEles);
 
 
+
+const hello = document.createElement('#div3');
+hello.innerHTML = "Hello....Hola....Bonjour";
+
+
+
+
+
+
+// function addEelement() {
+//     const greeting = document.createElement('#div1');
+//     greeting.innerHTML = "Hello....Hola....Bonjour";
+//     const container = document.querySelector('#div2');
+//     container.appendChild(greeting);
+//     container.innerHTML = " Why is this not working?";
+// }
