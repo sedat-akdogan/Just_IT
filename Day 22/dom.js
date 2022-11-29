88595c6dff1de1cfa1b7bb1aaed14d1a4ee20696
Sedat2121 <93882574+Sedat2121@users.noreply.github.com>
2022-11-29 14:01:16 +0000

    selecting by tag names

---
+++ Day 22/dom.js
@@ -0,0 +1,8 @@
+// Selecting by Tag names
+
+const myEles = document.getElementsByTagName("h1");
+document.getElementById("selected").innerHTML = "your selected greting is: " + myEles[0].innerHTML;
+
+console.log(myEles);
+
+
