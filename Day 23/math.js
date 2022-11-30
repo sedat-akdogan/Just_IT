7ebd60429da6737a515f759d99da42ecdd85bbde
Sedat2121 <93882574+Sedat2121@users.noreply.github.com>
2022-11-30 13:54:34 +0000

    round, floor, ceil, trunc, random

---
+++ Day 23/math.js
@@ -0,0 +1,12 @@
+console.log(Math.round(3.5));
+
+console.log(Math.floor(3.9));
+
+console.log(Math.ceil(5.1));
+
+console.log(Math.trunc(5.7));
+
+console.log(Math.random()); //between 0 and 1
+
+//You have to generate a whole number between 1 and 10
+
