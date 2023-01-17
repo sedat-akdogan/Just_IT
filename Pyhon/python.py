fe2942943e481cd06b5303ab9b1bcfde380fdafd
Sedat2121 <93882574+Sedat2121@users.noreply.github.com>
2023-01-17 22:07:17 +0000

    Just it python lesson

---
+++ Pyhon/python.py
@@ -0,0 +1,13 @@
+# Sequences
+
+"""
+This is a way
+of multi-line
+comment method.
+"""
+
+print("Hello World")
+
+print("Hello, my name is Sedat.")
+print("I am", + int(20 +19), "years old")
+print("I learn python all over again!!")
