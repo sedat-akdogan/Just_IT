97cee47c28f958f5e67cc3ae9d6131d2058c98f7
Sedat2121 <93882574+Sedat2121@users.noreply.github.com>
2023-01-19 16:03:59 +0000

    Python practice sesstion

--- Pyhon/python.py
+++ Pyhon/python.py
@@ -1,13 +1,23 @@
 # Sequences
 
-"""
-This is a way
-of multi-line
-comment method.
-"""
+# """
+# This is a way
+# of multi-line
+# comment method.
+# """
 
-print("Hello World")
+# print("Hello World")
 
-print("Hello, my name is Sedat.")
-print("I am", + int(20 +19), "years old")
-print("I learn python all over again!!")
+# print("Hello, my name is Sedat.")
+# print("I am", + int(20 +19), "years old")
+# print("I learn python all over again!!")
+
+
+def sum(num1, *args):
+    total = num1
+    for num in args:
+        total = total + num
+    return total
+
+sum(2, 5, 7, 8, 9)
+print(sum)
